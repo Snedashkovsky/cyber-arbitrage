- update Osmosis price calculation formula - add correct prices for Osmosis

src/data_extractors.py
@@ -4,6 +4,7 @@ import numpy as np
 from math import isnan
 from IPython.core.display import display, HTML
 from pandarallel import pandarallel
+from itertools import permutations
 
 from src.bash_utils import get_json_from_bash_query
 from config import IBC_COIN_NAMES, BOSTROM_RELATED_OSMO_POOLS, BOSTROM_POOLS_BASH_QUERY, OSMOSIS_POOLS_API_URL, \
@@ -106,14 +107,25 @@ def get_prices(pools_df: pd.DataFrame, display_data: bool = False) -> pd.DataFra
     _coins_unique_list = list(set(np.concatenate(_coins_list).flat))
     _price_df = pd.DataFrame(columns=_coins_unique_list, index=_coins_unique_list)
 
-    for _index, _pool_row in pools_df.iterrows():
+    _price_row_list = []
+    for _, _pool_row in pools_df.iterrows():
         _coins_pair = _pool_row.reserve_coin_denoms
-        _balances = {item['denom']: int(item['amount']) for item in _pool_row.balances}
+        _balances = \
+            {item['denom']: np.float64(item['amount']) / np.float64(item['weight']) if 'weight' in item.keys() else int(
+                item['amount'])
+             for item in _pool_row.balances}
         if _balances:
-            _price_df.loc[_coins_pair[0], _coins_pair[1]] = _balances[_coins_pair[0]] / _balances[_coins_pair[1]] * (
-                    1 - POOL_FEE)
-            _price_df.loc[_coins_pair[1], _coins_pair[0]] = _balances[_coins_pair[1]] / _balances[_coins_pair[0]] * (
-                    1 - POOL_FEE)
+            for _coin_from, _coin_to in permutations(_coins_pair, 2):
+                _price_row_list.append([
+                    _coin_from,
+                    _coin_to,
+                    _balances[_coin_from] * (1 - _pool_row.swap_fee),
+                    _balances[_coin_from] / _balances[_coin_to] * (1 - _pool_row.swap_fee)])
+        _price_overall_df = pd.DataFrame(_price_row_list, columns=['coin_from', 'coin_to', 'pool_balance', 'price'])
+        _price_overall_biggest_pools_df = \
+            _price_overall_df.sort_values('pool_balance').drop_duplicates(['coin_from', 'coin_to'], keep='last')
+        for _, _row in _price_overall_biggest_pools_df.iterrows():
+            _price_df.loc[_row.coin_from, _row.coin_to] = _row.price
     for _coin in _coins_unique_list:
         _price_df.loc[_coin, _coin] = 1
     _price_df.loc['uatom in bostrom', 'uatom in osmosis'] = 1