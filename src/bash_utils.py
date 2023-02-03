4d81fe63f9dfecb8a99a45aa55f1e7a878c7eece
Snedashkovsky <sn@cyberdrop.ai>
2023-02-03 12:45:18 +0700

    - refactor bash utils - refactor data extractors - recalculate Revenue model - recalculate Pools Liquidity

--- src/bash_utils.py
+++ src/bash_utils.py
@@ -2,7 +2,7 @@ import json
 from subprocess import Popen, PIPE
 
 
-def execute_bash(bash_command: str):
+def execute_bash(bash_command: str, shell: bool = False):
     if len(bash_command.split('"')) == 1:
         _bash_command_list = bash_command.split()
     elif len(bash_command.split('"')) == 2:
@@ -16,12 +16,13 @@ def execute_bash(bash_command: str):
             [item for items in bash_command.split('"')[2:] for item in items.split()]
     else:
         return None, f'Cannot split bash command {bash_command}'
-    popen_process = Popen(_bash_command_list, stdout=PIPE)
+    popen_process = Popen([bash_command], stdout=PIPE, shell=shell, text=True) \
+        if shell else Popen(_bash_command_list, stdout=PIPE)
     return popen_process.communicate(timeout=15)
 
 
-def get_json_from_bash_query(bash_command: str):
-    _res = execute_bash(bash_command)
-    if _res[0]:
-        return json.loads(_res[0].decode('utf8').replace("'", '"'))
+def get_json_from_bash_query(bash_command: str, shell: bool = False):
+    _res, _ = execute_bash(bash_command, shell=shell)
+    if _res:
+        return json.loads(_res.decode('utf8').replace("'", '"'))
     return
